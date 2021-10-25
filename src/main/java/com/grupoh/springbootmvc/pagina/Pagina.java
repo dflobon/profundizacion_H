f19d131e18869bd40e7f52c789b9f7fffd1e3c85
Alvaro Recalde <elvarox301@gmail.com>
2021-10-25 16:10:04 +0200

    Entidad Pagina actualizada

--- src/main/java/com/grupoh/springbootmvc/pagina/Pagina.java
+++ src/main/java/com/grupoh/springbootmvc/pagina/Pagina.java
@@ -1,9 +1,12 @@
 package com.grupoh.springbootmvc.pagina;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
-@Entity
-@Table
+@Entity(name = "Pagina")
+@Table(
+        name = "pagina"
+)
 public class Pagina {
     @Id
     @SequenceGenerator(
@@ -13,17 +16,77 @@ public class Pagina {
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence"
+            generator = "pagina_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
     )
     private Long id;
+
+    @Column(
+            name = "tipo",
+            updatable = true,
+            nullable = false
+    )
+    private String tipo;
+
+    @Column(
+            name = "pag_web",
+            updatable = true,
+            nullable = false
+    )
+    private String pagWeb;
+
+    @Column(
+            name = "titulo",
+            updatable = true,
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String titulo;
+
+    @Column(
+            name = "contenido",
+            updatable = true,
+            nullable = true,
+            columnDefinition = "TEXT"
+    )
+    private String contenido;
+
+    @Column(
+            name = "obsoleto",
+            updatable = true,
+            nullable = false
+    )
+    private Boolean obsoleto;
+
+    @Column(
+            name = "fecha",
+            updatable = false,
+            nullable = false
+    )
+    private LocalDate fecha;
+
+    @Column(
+            name = "imagen",
+            updatable = true,
+            nullable = true
+    )
+    private String imagen;
 
     public Pagina() {
 
     }
 
-    public Pagina(String titulo) {
+    public Pagina(String tipo, String pagWeb, String titulo, String contenido, String imagen) {
+        this.tipo = tipo;
+        this.pagWeb = pagWeb;
         this.titulo = titulo;
+        this.contenido = contenido;
+        this.imagen = imagen;
+        this.obsoleto = false;
+        this.fecha = LocalDate.now();
     }
 
     public Long getId() {
@@ -34,6 +97,22 @@ public class Pagina {
         this.id = id;
     }
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPagWeb() {
+        return pagWeb;
+    }
+
+    public void setPagWeb(String pagWeb) {
+        this.pagWeb = pagWeb;
+    }
+
     public String getTitulo() {
         return titulo;
     }
@@ -42,11 +121,49 @@ public class Pagina {
         this.titulo = titulo;
     }
 
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public Boolean getObsoleto() {
+        return obsoleto;
+    }
+
+    public void setObsoleto(Boolean obsoleto) {
+        this.obsoleto = obsoleto;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
     @Override
     public String toString() {
         return "Pagina{" +
                 "id=" + id +
+                ", tipo='" + tipo + '\'' +
+                ", pagWeb='" + pagWeb + '\'' +
                 ", titulo='" + titulo + '\'' +
+                ", contenido='" + contenido + '\'' +
+                ", obsoleto=" + obsoleto +
+                ", fecha=" + fecha +
+                ", imagen='" + imagen + '\'' +
                 '}';
     }
 }
