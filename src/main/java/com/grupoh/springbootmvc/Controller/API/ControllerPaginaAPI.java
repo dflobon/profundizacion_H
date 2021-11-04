9d943c3b2bee3cafd0283eea8004a0220b15021d
alvaro.recalde.alguacil <elvarox301@gmail.com>
2021-11-04 10:53:10 +0100

    Métodos GET (all y byid). closes #23

--- src/main/java/com/grupoh/springbootmvc/Controller/API/ControllerPaginaAPI.java
+++ src/main/java/com/grupoh/springbootmvc/Controller/API/ControllerPaginaAPI.java
@@ -1,8 +1,11 @@
 package com.grupoh.springbootmvc.Controller.API;
 
+import com.grupoh.springbootmvc.Entity.Pagina;
 import com.grupoh.springbootmvc.Service.PaginaService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -12,5 +15,15 @@ public class ControllerPaginaAPI {
 
     public ControllerPaginaAPI(PaginaService paginaService) {
         this.paginaService = paginaService;
+    }
+
+    @RequestMapping()
+    public List<Pagina> getPaginas() {
+        return paginaService.getAllPaginas();
+    }
+
+    @GetMapping("/api/{paginaId}")
+    public Optional<Pagina> getPaginaFromId(@PathVariable("paginaId") Long paginaId) {
+        return paginaService.getPaginaFromId(paginaId);
     }
 }
--- src/main/java/com/grupoh/springbootmvc/Service/PaginaService.java
+++ src/main/java/com/grupoh/springbootmvc/Service/PaginaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PaginaService {
@@ -36,4 +37,7 @@ public class PaginaService {
         return paginaRepository.findAll();
     }
 
+    public Optional<Pagina> getPaginaFromId(Long paginaId) {
+        return paginaRepository.findById(paginaId);
+    }
 }
