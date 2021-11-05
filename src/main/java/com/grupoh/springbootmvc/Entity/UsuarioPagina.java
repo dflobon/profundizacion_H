563f6dffcf943cbed0ff6f1e2da01225037f8eff
David Fdez <david.fernandez.lobon@alumnos.upm.es>
2021-11-05 18:25:17 +0100

    Modelo Relacion Usuario-Pagina añadido close #31

---
+++ src/main/java/com/grupoh/springbootmvc/Entity/UsuarioPagina.java
@@ -0,0 +1,38 @@
+package com.grupoh.springbootmvc.Entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity(name = "UsuarioPagina")
+@Table(name = "UsuarioPagina")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class UsuarioPagina {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToOne
+    private Usuario user_id;
+
+    @OneToOne
+    private Pagina pagina_id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UsuarioPagina that = (UsuarioPagina) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
