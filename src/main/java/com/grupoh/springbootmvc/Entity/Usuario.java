52a9da3d40ff8f0326fdd49f0566ffadbd6e9a18
David Fdez <david.fernandez.lobon@alumnos.upm.es>
2021-11-05 18:19:49 +0100

    Modelo Usuario añadido close #30

---
+++ src/main/java/com/grupoh/springbootmvc/Entity/Usuario.java
@@ -0,0 +1,44 @@
+package com.grupoh.springbootmvc.Entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity(name = "Usuario")
+@Table(name = "Usuario")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Usuario {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "mail", nullable = false)
+    private String mail;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Usuario usuario = (Usuario) o;
+        return id != null && Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
