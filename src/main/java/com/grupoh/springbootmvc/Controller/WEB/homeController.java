0532f19147630a1de7ab4812f8c66df3fd6c671f
pablo.gcalonge <pablogc1999@gmail.com>
2021-11-11 10:42:56 +0100

    homeController

--- src/main/java/com/grupoh/springbootmvc/Controller/WEB/homeController.java
+++ src/main/java/com/grupoh/springbootmvc/Controller/WEB/homeController.java
@@ -1,20 +1,23 @@
 package com.grupoh.springbootmvc.Controller.WEB;
 
+import com.grupoh.springbootmvc.Service.PaginaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
+@Controller
+@RequestMapping("/paginas")
 public class homeController {
-    @Controller
-    class HomeController {
+    private PaginaService paginaService;
+    public homeController(PaginaService paginaService) {this.paginaService = paginaService;}
 
-        @GetMapping("/")
-        String index(Principal principal) {
-            return principal != null ? "vistaDefault" : "logina";
-        }
-
+    @GetMapping
+    public String paginaIndex(Model model) {
+        model.addAttribute("paginas", paginaService.getAllPaginas());
+        return "vistaPaginaIndex";
     }
 }
